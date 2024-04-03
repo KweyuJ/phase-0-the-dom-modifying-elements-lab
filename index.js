0406d9ef142f16e523e2387fbca294e90e3e5a17
Joy <kweyuayieko@gmail.com>
2024-04-03 12:43:07 +0300

    Completed DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainE = document.getElementById("main");
+mainE.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Joy Kweyu is the champion";
+newHeader.id = "victory";
+
+document.body.append(newHeader);
